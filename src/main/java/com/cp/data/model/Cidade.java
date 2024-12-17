01adcca644b155fa608219411def937a5ac4616c
Jhonatan Santana Giacomasi Barros <112188407+jhonjiacomasi@users.noreply.github.com>
2024-12-17 19:21:28 -0300

    Adicionando construtor, equals e hascode

--- src/main/java/com/cp/data/model/Cidade.java
+++ src/main/java/com/cp/data/model/Cidade.java
@@ -1,37 +1,43 @@
-
-
-
 package com.cp.data.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.Set;
 
 import static jakarta.persistence.CascadeType.ALL;
 
-/**
- *
- * @author utfpr
- */
 @Entity
 @Data
 public class Cidade implements Serializable {
 
+    public Cidade() {}
 
-    public Cidade(){
-        //metodo exigido pela entity
+    public Cidade(String nome) {
+        this.nome = nome;
     }
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private int id;
+
     private String nome;
 
-
-    @OneToMany(cascade=ALL, mappedBy="cidade")
-   // @JsonBackReference
+    @OneToMany(cascade = ALL, mappedBy = "cidade")
     private Set<Pessoa> pessoas;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cidade cidade = (Cidade) o;
+        return id == cidade.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
